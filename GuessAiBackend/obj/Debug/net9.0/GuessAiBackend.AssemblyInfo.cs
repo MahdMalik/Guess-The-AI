da2860f32496b01a8e6dbf922c0ad5e4d3b9dba8
ai01010 <ashrafuli.us@gmail.com>
2025-06-09 18:12:02 -0500

    Commit 1.0.0

--- GuessAiBackend/obj/Debug/net9.0/GuessAiBackend.AssemblyInfo.cs
+++ GuessAiBackend/obj/Debug/net9.0/GuessAiBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GuessAiBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23035d0b55ce735f75b618c999b3bc7300c7e9f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81ed55dc60fa45ecf18bc8e79bd1175d8cc0fe64")]
 [assembly: System.Reflection.AssemblyProductAttribute("GuessAiBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GuessAiBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
