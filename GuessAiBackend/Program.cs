426523db023c72b2eb22c3d36958ecefb11bb119
MahdMalik <mahdmahdmalik@gmail.com>
2025-06-11 04:17:49 -0500

    alright, i've got the basic communication set up between the frontend and the backend. Yippee!

--- GuessAiBackend/Program.cs
+++ GuessAiBackend/Program.cs
@@ -3,7 +3,17 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddOpenApi();
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins("http://localhost:3000").AllowAnyHeader().AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
+
+app.UseCors();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -15,5 +25,15 @@ app.UseHttpsRedirection();
 
 // Minimal test endpoint
 app.MapGet("/", () => "Hello from your backend!");
+
+app.MapGet("/test", () =>
+{
+    string returnMessage = "Hi, backend here!";
+    return Results.Ok(new
+    {
+        success = true,
+        message = returnMessage
+    });
+});
 
 app.Run();
---
+++ app/queue/page.js
@@ -0,0 +1,26 @@
+'use client'
+import Image from "next/image";
+import { Button } from "@mui/material";
+import axios from "axios";
+
+export default function Home() {
+    
+    const testNetCall = async() => {
+        const response = await axios.get("http://localhost:5017/test")
+        const data = response.data
+        if(data.success)
+        {
+            console.log(data.message)
+        }
+        else
+        {
+            console.log("Communication failed!")
+        }
+    }
+    
+    return (
+        <div>
+            <Button variant="contained" onClick={testNetCall}>Click Here To Get Added to the Queue!</Button>
+        </div>
+    )
+}
