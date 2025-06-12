5b57c972406c45430e08dc75f9a34fad42d24aab
MahdMalik <mahdmahdmalik@gmail.com>
2025-06-12 17:34:37 -0500

    alright, now the only thing left is to, on enough players joining, start the damn match

--- GuessAiBackend/obj/Debug/net9.0/GuessAiBackend.AssemblyInfo.cs
+++ GuessAiBackend/obj/Debug/net9.0/GuessAiBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GuessAiBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cd27a427adfa768aeba977fa960fbd9c284546c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9011fbfde6db35a9ab08eacc6ce0299a7b1f8d9f")]
 [assembly: System.Reflection.AssemblyProductAttribute("GuessAiBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GuessAiBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- app/queue/page.js
+++ app/queue/page.js
@@ -9,6 +9,20 @@ export default function Home() {
     const [username, setName] = useState("walter")
     const socket = useRef(null);
 
+    //helper function to await getting am essage from the server through promises
+    const ReceiveMessage = () => {
+        return new Promise((resolve, reject) => {
+            socket.current.onmessage = (response) => {
+                const data = JSON.parse(response.data)
+                resolve(data)
+            }
+            socket.current.onerror = (err) => {
+                reject(err)
+            }
+        })
+    }
+
+    //helper function to await connection to a server through promises
     const ConnectToServer = () => {
         return new Promise ((resolve, reject) => {
             const socket = new WebSocket("ws://localhost:5017/ws")
@@ -27,15 +41,38 @@ export default function Home() {
         //means there's already a connection
         if(socket.current != null)
         {
+            //send message to leave
             socket.current.send(JSON.stringify({
                 username: username,
                 messageType: "Leave Queue",
                 queueType: "One Bot Game"
             }))
-            setQueueStatus(false);
+            //receive response, make sure we were actually able to leave
+            try
+            {
+                const data = await ReceiveMessage();
+                console.log(data)
+                //if succeeded in removal, set them no longer in the queue, close socket, reset ref
+                if(data.success)
+                {
+                    setQueueStatus(false)
+                    socket.current.close()
+                    socket.current = null
+                }
+                else
+                {
+                    alert("Something failed while attempting to leave queue: " + data.message)
+                }
+            }
+            catch(e)
+            {
+                alert("Error trying to receive mesasge")
+            }
         }
+        //means have to start connection
         else
         {
+            //try to connect
             try
             {
                 socket.current = await ConnectToServer();
@@ -45,15 +82,27 @@ export default function Home() {
                     messageType: "Join Queue",
                     queueType: "One Bot Game"
                 }))
-                setQueueStatus(true)
-                socket.current.onerror = (error) => {
-                    alert("Error with socket: " + error)
+                const data = await ReceiveMessage();
+                console.log(data)
+                //if able to connect, put on these listeners
+                if(data.success)
+                {
+                    setQueueStatus(true)
+                    socket.current.onerror = (error) => {
+                        alert("Error with socket: " + error)
+                    }
+                    socket.current.onclose = () => {
+                        console.log("Socket conneciton closed!")
+                        setQueueStatus(false);
+                        socket.current = null;
+                    }
                 }
-                socket.current.onclose = () => {
-                    console.log("Socket conneciton closed!")
-                    setQueueStatus(false);
-                    socket.current = null;
+                else
+                {
+                    setQueueStatus(false)
+                    socket.current.close()
                 }
+                
             }
             catch (e)
             {
@@ -65,9 +114,9 @@ export default function Home() {
     
     return (
         <div>
-            <Button variant="contained" onClick={toggleQueue}>Click Here To Get 
+            <Button variant="contained" onClick={toggleQueue}>Click Here To Get
                 {
-                    (inQueue) ? "Removed from" : "Added to"
+                    (inQueue) ? " Removed from " : " Added to "
                 } 
                 the Queue!
             </Button>
