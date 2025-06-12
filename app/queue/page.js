9011fbfde6db35a9ab08eacc6ce0299a7b1f8d9f
MahdMalik <mahdmahdmalik@gmail.com>
2025-06-12 16:51:49 -0500

    theoretically, frontend websocket stuff is done too. Now, time for testing, and after that actually coding matches

--- app/queue/page.js
+++ app/queue/page.js
@@ -2,27 +2,75 @@
 import Image from "next/image";
 import { Button } from "@mui/material";
 import axios from "axios";
+import { useEffect, useReducer, useState, useRef } from "react";
 
 export default function Home() {
-    
-    const joinQueue = async() => {
-        const response = await axios.get("http://localhost:5017/joinQueue", {
-            username: "walter"
+    const [inQueue, setQueueStatus] = useState(false);
+    const [username, setName] = useState("walter")
+    const socket = useRef(null);
+
+    const ConnectToServer = () => {
+        return new Promise ((resolve, reject) => {
+            const socket = new WebSocket("ws://localhost:5017/ws")
+            socket.onopen = () => {
+                resolve(socket)
+            }
+
+            socket.onerror = (err) => {
+                reject(err)
+            }
+
         })
-        const data = response.data
-        if(data.success)
+    }
+
+    const toggleQueue = async() => {
+        //means there's already a connection
+        if(socket.current != null)
         {
-            console.log(data.message)
+            socket.current.send(JSON.stringify({
+                username: username,
+                messageType: "Leave Queue",
+                queueType: "One Bot Game"
+            }))
+            setQueueStatus(false);
         }
         else
         {
-            console.log("Communication failed!")
+            try
+            {
+                socket.current = await ConnectToServer();
+                console.log("Socket connected!");
+                socket.current.send(JSON.stringify({
+                    username: username,
+                    messageType: "Join Queue",
+                    queueType: "One Bot Game"
+                }))
+                setQueueStatus(true)
+                socket.current.onerror = (error) => {
+                    alert("Error with socket: " + error)
+                }
+                socket.current.onclose = () => {
+                    console.log("Socket conneciton closed!")
+                    setQueueStatus(false);
+                    socket.current = null;
+                }
+            }
+            catch (e)
+            {
+                alert("Unable to make connection: " + e)
+            }
+
         }
     }
     
     return (
         <div>
-            <Button variant="contained" onClick={joinQueue}>Click Here To Get Added to the Queue!</Button>
+            <Button variant="contained" onClick={toggleQueue}>Click Here To Get 
+                {
+                    (inQueue) ? "Removed from" : "Added to"
+                } 
+                the Queue!
+            </Button>
         </div>
     )
 }
