25af7d06fe62b5436a90ecb37a5bf5ac790a5389
MahdMalik <mahdmahdmalik@gmail.com>
2025-07-22 00:54:34 -0500

    the task is done, yippee

--- GuessAiBackend/obj/Debug/net9.0/GuessAiBackend.AssemblyInfo.cs
+++ GuessAiBackend/obj/Debug/net9.0/GuessAiBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GuessAiBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c147315b4c14e38f22628ed9221b89d0726f916b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f315d61cb7ca3cd3443f299501b9a37dae01829")]
 [assembly: System.Reflection.AssemblyProductAttribute("GuessAiBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GuessAiBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
